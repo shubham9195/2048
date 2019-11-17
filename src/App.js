ee009dffb91dae39372a9238bbc02c1d3841feed
shubham_9195 <shubham@servitor.in>
2019-11-17 20:53:43 +0530

    2048 initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,336 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: null,
+      points: 0,
+      gameOver: false,
+      message: null
+    }
+  }
+  mainBoard() {
+    let board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+    board = this.placeRandom(this.placeRandom(board));
+    this.setState({ board, points: 0, gameOver: false, message: null });
+  }
 
-function App() {
+  getEmptyCoordinates(board) {
+    const blankCoordinates = [];
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board[r].length; c++) {
+        if (board[r][c] === 0) { blankCoordinates.push([r, c]) }
+      }
+    }
+
+    return blankCoordinates;
+  }
+  randomStartingNumber() {
+    const startingNumbers = [2, 4];
+    const randomNumber = startingNumbers[Math.floor(Math.random() * startingNumbers.length)];
+    return randomNumber;
+  }
+
+  placeRandom(board) {
+    const blankCoordinates = this.getEmptyCoordinates(board);
+    const randomCoordinate = blankCoordinates[Math.floor(Math.random() * blankCoordinates.length)];
+    const randomNumber = this.randomStartingNumber();
+    board[randomCoordinate[0]][randomCoordinate[1]] = randomNumber;
+    return board;
+  }
+
+  boardMoved(original, updated) {
+    return (JSON.stringify(updated) !== JSON.stringify(original)) ? true : false;
+  }
+
+  move(direction) {
+    if (!this.state.gameOver) {
+      if (direction === 'up') {
+        const movedUp = this.moveUp(this.state.board);
+        if (this.boardMoved(this.state.board, movedUp.board)) {
+          const upWithRandom = this.placeRandom(movedUp.board);
+
+          if (this.checkForGameOver(upWithRandom)) {
+            this.setState({ board: upWithRandom, gameOver: true, message: 'Game over!' });
+          } else {
+            this.setState({ board: upWithRandom, points: this.state.points += movedUp.points });
+          }
+        }
+      } else if (direction === 'right') {
+        const movedRight = this.moveRight(this.state.board);
+        if (this.boardMoved(this.state.board, movedRight.board)) {
+          const rightWithRandom = this.placeRandom(movedRight.board);
+
+          if (this.checkForGameOver(rightWithRandom)) {
+            this.setState({ board: rightWithRandom, gameOver: true, message: 'Game over!' });
+          } else {
+            this.setState({ board: rightWithRandom, points: this.state.points += movedRight.points });
+          }
+        }
+      } else if (direction === 'down') {
+        const movedDown = this.moveDown(this.state.board);
+        if (this.boardMoved(this.state.board, movedDown.board)) {
+          const downWithRandom = this.placeRandom(movedDown.board);
+
+          if (this.checkForGameOver(downWithRandom)) {
+            this.setState({ board: downWithRandom, gameOver: true, message: 'Game over!' });
+          } else {
+            this.setState({ board: downWithRandom, points: this.state.points += movedDown.points });
+          }
+        }
+      } else if (direction === 'left') {
+        const movedLeft = this.moveLeft(this.state.board);
+        if (this.boardMoved(this.state.board, movedLeft.board)) {
+          const leftWithRandom = this.placeRandom(movedLeft.board);
+
+          if (this.checkForGameOver(leftWithRandom)) {
+            this.setState({
+              board: leftWithRandom,
+              gameOver: true,
+              message: 'Game over!'
+            });
+          } else {
+            this.setState({ board: leftWithRandom, points: this.state.points += movedLeft.points });
+          }
+        }
+      }
+    } else {
+      this.setState({ message: 'Game over. Please start a new game.' });
+    }
+  }
+
+  moveUp(inputBoard) {
+    let rotatedRight = this.rotateRight(inputBoard);
+    let board = [];
+    let points = 0;
+
+    // Shift all numbers to the right
+    for (let r = 0; r < rotatedRight.length; r++) {
+      let row = [];
+      for (let c = 0; c < rotatedRight[r].length; c++) {
+        let current = rotatedRight[r][c];
+        (current === 0) ? row.unshift(current) : row.push(current);
+      }
+      board.push(row);
+    }
+    for (let r = 0; r < board.length; r++) {
+      for (let c = board[r].length - 1; c >= 0; c--) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c - 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c - 1] = 0;
+          points += board[r][c];
+        } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+          board[r][c] = board[r][c - 1];
+          board[r][c - 1] = 0;
+        }
+      }
+    }
+
+    board = this.rotateLeft(board);
+
+    return { board, points };
+  }
+
+  moveRight(inputBoard) {
+    let board = [];
+    let points = 0;
+
+    for (let r = 0; r < inputBoard.length; r++) {
+      let row = [];
+      for (let c = 0; c < inputBoard[r].length; c++) {
+        let current = inputBoard[r][c];
+        (current === 0) ? row.unshift(current) : row.push(current);
+      }
+      board.push(row);
+    }
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = board[r].length - 1; c >= 0; c--) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c - 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c - 1] = 0;
+          points += board[r][c];
+        } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+          board[r][c] = board[r][c - 1];
+          board[r][c - 1] = 0;
+        }
+      }
+    }
+
+    return { board, points };
+  }
+
+  moveDown(inputBoard) {
+    let rotatedRight = this.rotateRight(inputBoard);
+    let board = [];
+    let points = 0;
+
+    for (let r = 0; r < rotatedRight.length; r++) {
+      let row = [];
+      for (let c = rotatedRight[r].length - 1; c >= 0; c--) {
+        let current = rotatedRight[r][c];
+        (current === 0) ? row.push(current) : row.unshift(current);
+      }
+      board.push(row);
+    }
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c + 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c + 1] = 0;
+          points += board[r][c];
+        } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+          board[r][c] = board[r][c + 1];
+          board[r][c + 1] = 0;
+        }
+      }
+    }
+
+    
+    board = this.rotateLeft(board);
+
+    return { board, points };
+  }
+
+  moveLeft(inputBoard) {
+    let board = [];
+    let points = 0;
+
+    for (let r = 0; r < inputBoard.length; r++) {
+      let row = [];
+      for (let c = inputBoard[r].length - 1; c >= 0; c--) {
+        let current = inputBoard[r][c];
+        (current === 0) ? row.push(current) : row.unshift(current);
+      }
+      board.push(row);
+    }
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c + 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c + 1] = 0;
+          points += board[r][c];
+        } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+          board[r][c] = board[r][c + 1];
+          board[r][c + 1] = 0;
+        }
+      }
+    }
+
+    return { board, points };
+  }
+
+  rotateRight(matrix) {
+    let result = [];
+
+    for (let c = 0; c < matrix.length; c++) {
+      let row = [];
+      for (let r = matrix.length - 1; r >= 0; r--) {
+        row.push(matrix[r][c]);
+      }
+      result.push(row);
+    }
+
+    return result;
+  }
+
+  rotateLeft(matrix) {
+    let result = [];
+
+    for (let c = matrix.length - 1; c >= 0; c--) {
+      let row = [];
+      for (let r = matrix.length - 1; r >= 0; r--) {
+        row.unshift(matrix[r][c]);
+      }
+      result.push(row);
+    }
+
+    return result;
+  }
+  checkForGameOver(board) {
+    let moves = [
+      this.boardMoved(board, this.moveUp(board).board),
+      this.boardMoved(board, this.moveRight(board).board),
+      this.boardMoved(board, this.moveDown(board).board),
+      this.boardMoved(board, this.moveLeft(board).board)
+    ];
+
+    return (moves.includes(true)) ? false : true;
+  }
+  UNSAFE_componentWillMount() {
+    this.mainBoard();
+    const body = document.querySelector('body');
+    body.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  handleKeyDown(e) {
+    const up = 38;
+    const right = 39;
+    const down = 40;
+    const left = 37
+    const n = 78;
+
+    if (e.keyCode === up) {
+      this.move('up');
+    } else if (e.keyCode === right) {
+      this.move('right');
+    } else if (e.keyCode === down) {
+      this.move('down');
+    } else if (e.keyCode === left) {
+      this.move('left');
+    } else if (e.keyCode === n) {
+      this.mainBoard();
+    }
+  }
+  render() {
+    console.log('check kro', this.state.board);
+    return (
+      <div>
+        <div className="button" onClick={() => { this.mainBoard() }}>New Game</div>
+          <div className="buttons">
+          <div className="button" onClick={() => { this.move('up') }}>Up</div>
+          <div className="button" onClick={() => { this.move('right') }}>Right</div>
+          <div className="button" onClick={() => { this.move('down') }}>Down</div>
+          <div className="button" onClick={() => { this.move('left') }}>Left</div>
+        </div>
+        <div className="score">points: {this.state.points}</div>
+
+        <table>
+          {this.state.board.map((row, i) => (<Row key={i} row={row} />))}
+        </table>
+
+        <p>{this.state.message}</p>
+      </div>
+    );
+
+  }
+};
+const Row = ({ row }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <tr>
+      {row.map((cell, i) => (<Cell key={i} cellValue={cell} />))}
+    </tr>
   );
-}
+};
+
+const Cell = ({ cellValue }) => {
+  let color = 'cell';
+  let value = (cellValue === 0) ? '' : cellValue;
+  if (value) {
+    color += ` color-${value}`;
+  }
+
+  return (
+    <td>
+      <div className={color}>
+        <div className="number">{value}</div>
+      </div>
+    </td>
+  );
+};
 
 export default App;
